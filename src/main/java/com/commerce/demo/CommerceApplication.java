2e7c40106826da72bb0b6461123a8bd48c481bbd
ttiago7 <c.ttiago7@gmail.copm>
2021-05-20 07:08:46 -0300

    add images to documentation readme

--- src/main/java/com/commerce/demo/CommerceApplication.java
+++ src/main/java/com/commerce/demo/CommerceApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
+@Configuration
 @EnableAsync
 public class CommerceApplication {
 
